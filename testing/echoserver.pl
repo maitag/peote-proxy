87322756d2a0ba73e5ec6f82e0d6f6811d5daa03
Sylvio Sell <info@maitag.de>
2018-02-15 07:54:09 +0100

    errorhandler in echotestserver

--- testing/echoserver.pl
+++ testing/echoserver.pl
@@ -7,18 +7,25 @@ use strict;
 
 # Include POE and POE::Component::Server::TCP.
 
-use POE qw(Component::Server::TCP);
+use POE qw(Component::Server::TCP Filter::Stream);
 
 # Start a TCP server.  Client input will be logged to the console and
 # echoed back to the client, one line at a time.
 
 POE::Component::Server::TCP->new(
-  Alias       => "echo_server",
-  Port        => 7685,
+  #Alias       => "echo_server",
+  Hostname => 'localhost',
+  #Address => '127.0.0.1',
+  Port        => 7680,
+  ClientFilter => "POE::Filter::Stream",
   ClientInput => sub {
     my ($session, $heap, $input) = @_[SESSION, HEAP, ARG0];
-    print "Session ", $session->ID(), " got input: $input\n";
+    # print "Session ", $session->ID(), " got input: ".length($input)."\n";
     $heap->{client}->put($input);
+  },
+  Error => sub {
+    my ($syscall_name, $err_num, $err_str) = @_[ARG0..ARG2];
+    print "ERROR: ", $syscall_name, $err_num, $err_str;
   }
 );
 
